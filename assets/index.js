fe3f5f3b0c854638cd337ea24be7bda68f6b4869
gilberto <gilbertosa099@gmail.com>
2022-06-11 16:36:03 -0500

    [ADD]More styles config

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,15 @@
 const baseurl = "https://rickandmortyapi.com/api";
 let container = document.getElementById("main-content");
-container.style =
-  "display: flex; width: 100%;flex-wrap: wrap;justify-content: center;align-items: center;align-content: center";
 let results = [];
 fetch(`${baseurl}/character`)
   .then((res) => res.json())
   .then((data) => {
     data.results.forEach((element) => {
       const nodeCard = document.createElement("div");
-      nodeCard.style='border: solid 1px ; border-radius: 12px; margin:8px; padding: 8px'
-      const nodeItem = document.createElement("h2");
+      nodeCard.className = "card";
+      const nodeItem = document.createElement("h3");
       const nodeImage = document.createElement("img");
+      nodeImage.loading = "lazy";
       //const nameText = document.createTextNode(element.name);
       //node.appendChild(nameText);
       //the two commented lines, can be just one
