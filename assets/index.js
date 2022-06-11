2cca87c6a9c58645a96b549581fd890b5b2979e1
gilberto <gilbertosa099@gmail.com>
2022-06-10 21:04:01 -0500

    [ADD]Images and div container

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,20 @@
 const baseurl = "https://rickandmortyapi.com/api";
-const container = document.getElementById("main-content");
+let container = document.getElementById("main-content");
+let results = [];
 fetch(`${baseurl}/character`)
   .then((res) => res.json())
-  .then((data) =>
+  .then((data) => {
     data.results.forEach((element) => {
-      const node = document.createElement("h2");
-      node.appendChild(document.createTextNode(element.name));
-      container.appendChild(node);
-    })
-  );
+        const nodeCard=document.createElement('div');
+      const nodeItem = document.createElement("h2");
+      const nodeImage = document.createElement("img");
+      //const nameText = document.createTextNode(element.name);
+      //node.appendChild(nameText);
+      //the two commented lines, can be just one
+      nodeItem.textContent = element.name;
+      nodeImage.src = element.image;
+      nodeCard.append(nodeItem,nodeImage)
+      results.push(nodeCard);
+    });
+    container.append(...results);
+  });
