0b48b7910c2d8dafbea38079f3b10d9cf3c600fa
gilberto <gilbertosa099@gmail.com>
2022-06-11 16:01:44 -0500

    [ADD]Initial styles

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,14 @@
 const baseurl = "https://rickandmortyapi.com/api";
 let container = document.getElementById("main-content");
+container.style =
+  "display: flex; width: 100%;flex-wrap: wrap;justify-content: center;align-items: center;align-content: center";
 let results = [];
 fetch(`${baseurl}/character`)
   .then((res) => res.json())
   .then((data) => {
     data.results.forEach((element) => {
-        const nodeCard=document.createElement('div');
+      const nodeCard = document.createElement("div");
+      nodeCard.style='border: solid 1px ; border-radius: 12px; margin:8px; padding: 8px'
       const nodeItem = document.createElement("h2");
       const nodeImage = document.createElement("img");
       //const nameText = document.createTextNode(element.name);
@@ -13,7 +16,8 @@ fetch(`${baseurl}/character`)
       //the two commented lines, can be just one
       nodeItem.textContent = element.name;
       nodeImage.src = element.image;
-      nodeCard.append(nodeItem,nodeImage)
+      nodeImage.alt = `${element.name}-image`;
+      nodeCard.append(nodeItem, nodeImage);
       results.push(nodeCard);
     });
     container.append(...results);
