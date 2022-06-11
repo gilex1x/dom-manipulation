4f39b90183cd185f5ae092fa7d73183753826566
gilberto <gilbertosa099@gmail.com>
2022-06-11 17:05:11 -0500

    [ADD]stlyles and some functions to delegate responsabilities

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,27 @@
 const baseurl = "https://rickandmortyapi.com/api";
 let container = document.getElementById("main-content");
 let results = [];
+
+const createNodeElement = (item) => {
+  const nodeCard = document.createElement("div");
+  nodeCard.className = "card";
+  const nodeItem = document.createElement("h3");
+  const nodeImage = document.createElement("img");
+  nodeImage.loading = "lazy";
+  //const nameText = document.createTextNode(element.name);
+  //node.appendChild(nameText);
+  //the two commented lines, can be just one
+  nodeItem.textContent = item.name;
+  nodeImage.src = item.image;
+  nodeImage.alt = `${item.name}-image`;
+  nodeCard.append(nodeItem, nodeImage);
+  return nodeCard;
+};
 fetch(`${baseurl}/character`)
   .then((res) => res.json())
   .then((data) => {
     data.results.forEach((element) => {
-      const nodeCard = document.createElement("div");
-      nodeCard.className = "card";
-      const nodeItem = document.createElement("h3");
-      const nodeImage = document.createElement("img");
-      nodeImage.loading = "lazy";
-      //const nameText = document.createTextNode(element.name);
-      //node.appendChild(nameText);
-      //the two commented lines, can be just one
-      nodeItem.textContent = element.name;
-      nodeImage.src = element.image;
-      nodeImage.alt = `${element.name}-image`;
-      nodeCard.append(nodeItem, nodeImage);
-      results.push(nodeCard);
+      results.push(createNodeElement(element));
     });
     container.append(...results);
   });
