534ddc25227b9b523366e40470cf21f56c47c808
gilberto <gilbertosa099@gmail.com>
2022-06-12 18:00:27 -0500

    [add]lazy load

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,34 @@
+import { addImage } from "./lazy.js";
 const baseurl = "https://rickandmortyapi.com/api";
 let container = document.getElementById("main-content");
 let results = [];
 
-container.addEventListener("click", (event) => {
-  event.target.nodeName == "H3" &&
-    console.log("Click desde", event.target.className);
-});
+// container.addEventListener("click", (event) => {
+//   event.target.nodeName == "H3" &&
+//     console.log("Click desde", event.target.className);
+// });
 
 const createNodeElement = (item) => {
   const nodeCard = document.createElement("div");
   nodeCard.className = "card";
   nodeCard.id = `${item.name.replace(/ /g, "-")}`;
   const nodeItem = document.createElement("h3");
+  const loaderNode = document.createElement("div");
+  loaderNode.style =
+    "height: 300px; width: 300px; background-color:grey; border-radius: 12px;";
+  loaderNode.className = "wrapper";
   const nodeImage = document.createElement("img");
   nodeImage.loading = "lazy";
   //const nameText = document.createTextNode(element.name);
   //node.appendChild(nameText);
   //the two commented lines, can be just one
   nodeItem.textContent = item.name;
-  nodeImage.src = item.image;
-  nodeImage.alt = `${item.name}-image`;
-  nodeCard.append(nodeItem, nodeImage);
+  //pre-set el src de la img
+  nodeImage.dataset.src = item.image;
+  nodeImage.alt = `${item.name.replace(/ /g, "-")}-image`;
+  loaderNode.appendChild(nodeImage);
+  nodeCard.append(nodeItem, loaderNode);
+  addImage(nodeCard);
   return nodeCard;
 };
 fetch(`${baseurl}/character`)
---
+++ assets/lazy.js
@@ -0,0 +1,23 @@
+//me dice si el elemento es visible o no
+const isVisible = (entry) => {
+  return entry.isIntersecting;
+};
+//la acción que se ejecuta al ser visible mi elemento
+const loadImage = (entri) => {
+  const card = entri.target;
+  const imageNode = card.querySelector("img");
+  //debugger;
+  console.log(imageNode.dataset.src);
+  imageNode.src = imageNode.dataset.src;
+
+  //remover observer
+  observer.unobserve(card);
+};
+//creo una instancia del IntersectionObserver y le paso un callback
+const observer = new IntersectionObserver((entries) => {
+  entries.filter(isVisible).forEach(loadImage);
+});
+//asignamos el observer a un elemento
+export const addImage = (image) => {
+  observer.observe(image);
+};
