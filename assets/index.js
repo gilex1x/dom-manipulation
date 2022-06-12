d1ef9df1a5c100d1aa8061d748c3f378ad05158d
gilberto <gilbertosa099@gmail.com>
2022-06-12 18:32:33 -0500

    [ADD]Load more button

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,20 @@
 import { addImage } from "./lazy.js";
 const baseurl = "https://rickandmortyapi.com/api";
 let container = document.getElementById("main-content");
+let loadButton = document.getElementById("loadButton");
 let results = [];
+let page = 1;
 
-// container.addEventListener("click", (event) => {
-//   event.target.nodeName == "H3" &&
-//     console.log("Click desde", event.target.className);
-// });
+loadButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (page == 42) {
+    page = 1;
+  } else {
+    page = page + 1;
+  }
+
+  getData();
+});
 
 const createNodeElement = (item) => {
   const nodeCard = document.createElement("div");
@@ -31,11 +39,16 @@ const createNodeElement = (item) => {
   addImage(nodeCard);
   return nodeCard;
 };
-fetch(`${baseurl}/character`)
-  .then((res) => res.json())
-  .then((data) => {
-    data.results.forEach((element) => {
-      results.push(createNodeElement(element));
+
+const getData = async () => {
+  await fetch(`${baseurl}/character/?page=${page}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.results.forEach((element) => {
+        results.push(createNodeElement(element));
+      });
+      container.append(...results);
     });
-    container.append(...results);
-  });
+};
+
+window.onload = getData();
